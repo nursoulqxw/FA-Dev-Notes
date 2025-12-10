bb7433191b0139d737b287ebe000040cac3c34aa
Nurislam Aliyev <nursoulqxw@gmail.com>
2025-12-10 22:46:34 +0500

    added declare_request_example_data

---
+++ src/user-guide/dec_req_exp_data.py
@@ -0,0 +1,123 @@
+from fastapi import FastAPI, Body
+
+from typing import Annotated
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+#---------We can make it to data look like this, so json data would get info like this to the app-----------------
+    # model_config = {
+    #     "json_schema_extra": {
+    #         "examples": [
+    #             {
+    #                 "name" : "Foo",
+    #                 "description": "A very nice Item",
+    #                 "price": 35.4,
+    #                 "tax": 3.2,
+    #             }
+    #         ]
+    #     }
+    # }
+
+
+#--------------Body with multiple examples-------------------
+@app.put("/items/{item_id}")
+async def update_item(
+    *,
+    item_id:int,
+    item: Annotated[
+        Item,
+        Body(
+            openapi_examples = {
+                "normal": {
+                    "summary": "A normal example",
+                    "description": "A **normal** item works correctly.",
+                    "value": {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    },
+                },
+                "converted":{
+                    "summary": "An example with converted data",
+                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                    "value":{
+                        "name": "Bar",
+                        "price": "35.4",
+                    },
+                },
+                "invalid": {
+                    "summary": "Invalid data is rejected with an error",
+                    "value":{
+                        "name": "Baz",
+                        "price": "thirty five point four",
+                    },
+                },
+            },
+        ),
+    ],
+):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+
+
+
+
+# name: str = Field(examples=["Foo"])
+# description: str | None = Field(default=None, examples=["A very nice Item"])
+# price: float = Field(examples=[35.4])
+# tax: float | None = Field(default=None, examples=[3.2])
+
+
+
+
+#---------------Multiple Examples Body --------------------------
+# Body(
+#             examples=[
+#                 {
+#                     "name": "Foo",
+#                     "description": "A very nice Item",
+#                     "price": 35.4,
+#                     "tax": 3.2,
+#                 },
+#                 {
+#                     "name": "Bar",
+#                     "price": "35.4",
+#                 },
+#                 {
+#                     "name": "Baz",
+#                     "price": "thirty five point four"
+#                 }
+#             ],
+#         ),
+
+
+
+
+#----------------SINGLE EXAMPLE---------------
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     item_id:int,
+#     item: Annotated[
+#         Item,
+#         Body(
+#             examples=[
+#                 {
+#                     "name": "Foo",
+#                     "description": "A very nice Item",
+#                     "price": 35.4,
+#                     "tax": 3.2,
+#                 }
+#             ],
+#         ),
+#     ],
+# ):
