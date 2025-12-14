1177581249617a9d0a18a1250155bf2caeb1e36e
Nurislam Aliyev <nursoulqxw@gmail.com>
2025-12-15 04:25:50 +0500

    added cookie and header param models

---
+++ src/user-guide/cookie_models.py
@@ -0,0 +1,17 @@
+from typing import Annotated
+from pydantic import BaseModel 
+
+from fastapi import FastAPI, Cookie
+
+app = FastAPI()
+
+class Cookies(BaseModel):
+    model_config = {"extra": "forbid"}
+
+    session_id: str
+    fatebook_tracker: str | None = None
+    googall_tracker: str | None = None 
+
+@app.get("/items/")
+async def read_items(cookies: Annotated[Cookies, Cookie()]):
+    return cookies
