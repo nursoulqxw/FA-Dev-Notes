3dff21355c480d70cf36bea0f6528e22d789e098
Nurislam Aliyev <alievnurislam0@gmail.com>
2026-02-02 22:49:30 +0500

    just added routers

--- src/user-guide/sub_app/main.py
+++ src/user-guide/sub_app/main.py
@@ -3,9 +3,10 @@ from fastapi import Depends, FastAPI
 from .dependencies import get_token_header, get_query_token
 
 # todo: import routes
+from .routers.users import router
 
 app = FastAPI(dependencies=[Depends(get_query_token)])
-
+app.include_router(router)
 
 @app.get("/")
 async def root():
