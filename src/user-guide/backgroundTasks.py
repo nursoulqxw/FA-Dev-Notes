077b3135e7e57d3eb49e536616eb5d6f1ff888ea
Nurislam Aliyev <alievnurislam0@gmail.com>
2026-02-07 13:44:56 +0500

    dont have time, will complete in 2 hours

--- src/user-guide/backgroundTasks.py
+++ src/user-guide/backgroundTasks.py
@@ -1,19 +1,24 @@
 import time
 from fastapi import FastAPI, BackgroundTasks, status
-from pydantic import BaseModel
+from typing import Annotated
 
 
 app = FastAPI()
 
-def write_notification(email: str, message = ""):
-    with open('log.txt', mode='w') as email_file:
-        content = f"notication  for {email}: {message}"
-        time.sleep(5)
-        email_file.write(content)
+# def write_notification(email: str, message = ""):
+#     with open('log.txt', mode='w') as email_file:
+#         content = f"notication  for {email}: {message}"
+#         time.sleep(5)
+#         email_file.write(content)
 
 
-@app.post("/send-notification/{email}", status_code=status.HTTP_202_ACCEPTED)
-async def send_notification(email: str, background_tasks: BackgroundTasks):
-    background_tasks.add_task(write_notification, email, message="some notification")
-    return {"message": "Notification sent in the background"}
+# @app.post("/send-notification/{email}", status_code=status.HTTP_202_ACCEPTED)
+# async def send_notification(email: str, background_tasks: BackgroundTasks):
+#     background_tasks.add_task(write_notification, email, message="some notification")
+#     return {"message": "Notification sent in the background"}
+
+# 'a' - writes it to the end
+def write_log(message: str):
+    with open('log.txt', mode='a') as log:
+        log.write(message)
 
