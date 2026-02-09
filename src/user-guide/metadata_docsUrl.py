d8cccf361f1e3bb69899d2ef1e85d9776e5fdaa4
Nurislam Aliyev <alievnurislam0@gmail.com>
2026-02-10 00:42:59 +0500

    completed the metadata and docs url chapter

--- src/user-guide/metadata_docsUrl.py
+++ src/user-guide/metadata_docsUrl.py
@@ -16,8 +16,44 @@ you will be able to:
 
 """
 
+tags_metadata = [
+    dict(name='users', 
+          description="operations with users. the **login** logic is also here."
+    ),
+
+    dict(name='items',
+         description="Manage items. So _fancy_ they have their own docs",
+         externalDocs=dict(
+             description="Items external docs",
+             url="https://www.jvp.design"
+         )
+    ),
+]
+
 app = FastAPI(
     title="Chimichang app",
     description=description,
-    version="0.0.1"
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact=dict(
+        name="Deadpoolio the Amazing",
+        url="http://x-force.example.com/contact",
+        email="dp@x-force.example.com",
+    ),
+    license_info=dict(
+        name="Apache 2.0", url="https://www.apache.org/licenses/LICENSE-2.0.html"
+    ),
+    openapi_tags=tags_metadata,
+    openapi_url="/api/v1/openapi.json",
+    # docs_url="/hello-world",
+    # redoc_url=None,
+
 )
+
+@app.get('/users', tags=['users'])
+async def get_users():
+    return [dict(name="Harry"), dict(name="Ron")]
+
+@app.get("/items", tags=['items'])
+async def read_items():
+    return [dict(name = "wand"), dict(name="flying broom")]
