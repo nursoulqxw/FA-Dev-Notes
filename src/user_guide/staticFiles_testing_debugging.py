cb4806f8a9e415430ccfb757ee30c48dc9081865
Nurislam Aliyev <alievnurislam0@gmail.com>
2026-03-08 02:59:29 +0500

    finally completed the fastapi user guide tutor

--- src/user_guide/staticFiles_testing_debugging.py
+++ src/user_guide/staticFiles_testing_debugging.py
@@ -1,4 +1,6 @@
+import uvicorn
 from fastapi import FastAPI, HTTPException, status, Header
+from typing import Annotated
 # from typing import H
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
@@ -42,22 +44,22 @@ class Item(BaseModel):
 
 
 @app.get("/items/{item_id}",  response_model=Item)
-async def read_main(item_id: str, x_token: str = Header(...)):
+async def read_main(item_id: str, x_token: Annotated[str, Header()]):
     if x_token != fake_secret_token: 
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail = "Invalid X-Token header")
     # item=fake_db.get(item_id)
     # if item is None
     if item_id not in fake_db:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = "Item not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = "item not found")
     return fake_db[item_id]
 
 
-@app.post("/items/{item_id}", response_model=Item)
-async def create_item(item: Item, x_token: str = Header(...)):
+@app.post("/items/", response_model=Item, status_code=status.HTTP_201_CREATED)
+async def create_item(item: Item, x_token: Annotated[str, Header()]) -> Item:
     if x_token != fake_secret_token: 
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail = "Invalid X-Token header")
     if item.id in fake_db:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Item already exists")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="item already exists")
     
     fake_db[item.id] = item
     return item
--- src/user_guide/test_main.py
+++ src/user_guide/test_main.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient   
+from fastapi import status
 # import sys
 # from pathlib import Path
 
@@ -11,7 +12,7 @@ client = TestClient(app)
 
 def test_read_item():
     response = client.get("/items/foo", headers={"X-Token": "coneofsilence"})
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     assert response.json() == {
         "id": "foo",
         "title": "Foo",
@@ -21,13 +22,41 @@ def test_read_item():
 
 def test_read_item_bad_token():
     response = client.get("/items/too", headers={"X-Token": "wrong"})
-    assert response.status_code == 400
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.json() == {"detail": "Invalid X-Token header"}
 
 def test_read_inexisting_item():
-    response = client.get("/items/baz", headers={"X-Token", "coneofsilence"})
-    assert response.status_cide == 404
+    response = client.get("/items/baz", headers={"X-Token": "coneofsilence"})
+    assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {"detail": "item not found"}
 
 def test_create_item():
-    ...
+    response = client.post(
+        "/items/",
+        json = {"id": "foobar", "title": "Foo Bar", "description": "The Foo Bartender"},
+        headers={"X-Token": "coneofsilence"},
+    )
+    assert response.status_code==status.HTTP_201_CREATED
+    assert response.json() == {
+        "id":"foobar",
+        "title": "Foo Bar",
+        "description": "The Foo Bartender",
+
+    }
+
+def test_create_item_bad_token():
+    response = client.post(
+        "/items/",
+        headers={"X-Token": "badheader"}, 
+        json={"id":"bazz", "title": "Bazz", "description":"drop the bazz"}
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {"detail": "Invalid X-Token header"}
+
+
+def test_create_existing_item():
+    response = client.post("/items/",
+        headers={"X-Token": "coneofsilence"}, 
+        json={"id":"foo", "title": "Bazz", "description":"drop the bazz"})
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {"detail": "item already exists"}
